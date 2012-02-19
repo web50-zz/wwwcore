41536d513ddd58fe30843d8f30070e099ad611f0
Anthon S Litvinenko <a.litvinenko@web50.ru>
2012-02-20 00:11:47 +0400

    Changed UI Text configuration form

--- var/ui/text/configure_form.js
+++ var/ui/text/configure_form.js
@@ -1,10 +1,10 @@
-ui.text.configure_form = function(config){
-	Ext.apply(this, config);
-	this.Load = function(data){
+ui.text.configure_form = Ext.extend(Ext.form.FormPanel, {
+	Load: function(data){
 		var f = this.getForm();
 		f.setValues(Ext.decode(data));
-	}
-	var Save = function(){
+	},
+
+	Save: function(){
 		var f = this.getForm();
 		if (f.isValid()){
 			var config = {};
@@ -15,40 +15,81 @@ ui.text.configure_form = function(config){
 			}
 			this.fireEvent('saved', config);
 		}
-	}.createDelegate(this);
-	var Cancel = function(){
+	},
+	
+	Cancel: function(){
 		this.fireEvent('cancelled');
-	}.createDelegate(this);
-	ui.text.configure_form.superclass.constructor.call(this, {
-		frame: true, 
-		defaults: {xtype: 'textfield', width: 100, anchor: '100%'},
-		items: [
-			new Ext.form.ComboBox({
-				store: new Ext.data.JsonStore({url: 'di/text/available.json', fields: ['id', 'title'], autoLoad: true}),
-				fieldLabel: this.labelType, hiddenName: '_sid',
-				valueField: 'id', displayField: 'title', triggerAction: 'all', selectOnFocus: true, editable: false
-			})
-		],
-		buttonAlign: 'right',
-		buttons: [
-			{iconCls: 'accept', text: this.bttSave, handler: Save},
-			{iconCls: 'cancel', text: this.bttCancel, handler: Cancel}
-		]
-	});
-	this.addEvents(
-		"saved",
-		"cancelled"
-	);
-	this.on({
-		saved: function(data){
-			this.getForm().setValues([{id: '_sid', value: data.id}]);
-		},
-		scope: this
-	})
-}
-Ext.extend(ui.text.configure_form , Ext.form.FormPanel, {
-	labelType: 'Текст',
+	},
 
+	lblContent: 'Контент',
+	lblTitleHide: 'Скрыть заголовок',
 	bttSave: 'Применить',
 	bttCancel: 'Отмена',
+	errInputText: 'Корректно заполните все необходимые поля',
+
+	/**
+	 * @constructor
+	 */
+	constructor: function(config){
+		config = config || {};
+		Ext.apply(this, {
+			formWidth: 400,
+			formHeight: 140
+		});
+		Ext.apply(this, {
+			frame: true,
+			border: false,
+			labelAlign: 'right', 
+			labelWidth: 140,
+			defaults: {xtype: 'textfield', width: 150, anchor: '100%'},
+			//layout: 'fit',
+			items: [
+				{fieldLabel: this.lblContent, hiddenName: '_sid', xtype: 'combo',
+					store: new Ext.data.JsonStore({url: 'di/text/available.json', fields: ['id', 'title'], autoLoad: true}),
+					valueField: 'id', displayField: 'title',
+					triggerAction: 'all', selectOnFocus: true, editable: false
+				},
+				{fieldLabel: this.lblTitleHide, hiddenName: 'hide_title', xtype: 'combo', width: 50,
+					store: new Ext.data.SimpleStore({fields: ['value', 'title'], data: [[0, 'Нет'], [1, 'Да']]}),
+					valueField: 'value', displayField: 'title', value: 0,
+					mode: 'local', triggerAction: 'all', selectOnFocus: true, editable: false
+				}
+			],
+			buttonAlign: 'right',
+			buttons: [
+				{iconCls: 'disk', text: this.bttSave, handler: this.Save, scope: this},
+				{iconCls: 'cancel', text: this.bttCancel, handler: this.Cancel, scope: this}
+			],
+			keys: [
+				{key: [Ext.EventObject.ENTER], handler: this.Save, scope: this}
+			]
+		});
+		Ext.apply(this, config);
+		ui.text.configure_form.superclass.constructor.call(this, config);
+		this.on({
+			scope: this
+		})
+	},
+
+	/**
+	 * To manually set default properties.
+	 * 
+	 * @param {Object} config Object containing all config options.
+	 */
+	configure: function(config){
+		config = config || {};
+		Ext.apply(this, config, config);
+	},
+
+	/**
+	 * @private
+	 * @param {Object} o Object containing all options.
+	 *
+	 * Initializes the box by inserting into DOM.
+	 */
+	init: function(o){
+	},
+
+	reloadServices: function(data, id){
+	}
 });

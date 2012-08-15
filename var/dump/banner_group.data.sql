c7543fc6781d6b5420a706fd7847c4be2c368e20
Fedot B Pozdnyakov 9* <9@u9.ru>
2012-08-15 15:10:39 +0400

    banner_group initial data dump corrected

--- var/dump/banner_group.data.sql
+++ var/dump/banner_group.data.sql
@@ -1,5 +1,5 @@
-LOCK TABLES `structure` WRITE;
-/*!40000 ALTER TABLE `structure` DISABLE KEYS */;
-INSERT INTO `structure` VALUES (1,'root','',0,0,1,2,1);
-/*!40000 ALTER TABLE `structure` ENABLE KEYS */;
+LOCK TABLES `banner_group` WRITE;
+/*!40000 ALTER TABLE `banner_group` DISABLE KEYS */;
+INSERT INTO `banner_group` VALUES (1,'root','',0,0,1,2,1);
+/*!40000 ALTER TABLE `banner_group` ENABLE KEYS */;
 UNLOCK TABLES;

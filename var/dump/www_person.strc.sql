a7227e69ede2ad58014241ab719db0d73ced86dc
Anthon S Litvinenko <a.litvinenko@web50.ru>
2012-12-12 17:45:41 +0400

    Added new module WWW Persons

---
+++ var/dump/www_person.strc.sql
@@ -0,0 +1,11 @@
+CREATE TABLE `www_person` (
+	`id` smallint(5) unsigned NOT NULL AUTO_INCREMENT,
+	`order` smallint(5) unsigned NOT NULL,
+	`person_title` varchar(255) NOT NULL,
+	`person_name` varchar(128) NOT NULL,
+	`real_name` varchar(64) NOT NULL,
+	`comment` varchar(255) NOT NULL,
+	`description` text NOT NULL,
+	PRIMARY KEY (`id`),
+	KEY `order` (`order`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;

0712f8cc5dc2420d55b4988dde95594ab89d5cac
Anthon S Litvinenko <a.litvinenko@web50.ru>
2013-01-25 19:01:44 +0400

    Added new module for computing article rating

--- var/dump/article.strc.sql
+++ var/dump/article.strc.sql
@@ -1,12 +1,20 @@
 CREATE TABLE `article` (
-`id` mediumint(8) unsigned NOT NULL AUTO_INCREMENT,
-`release_date` date NOT NULL DEFAULT '0000-00-00',
-`title` varchar(255) NOT NULL DEFAULT '',
-`author` varchar(255) NOT NULL DEFAULT '',
-`source` varchar(255) NOT NULL,
-`content` text NOT NULL,
-`category` int(10) unsigned NOT NULL DEFAULT '0',
-`image` varchar(255) NOT NULL DEFAULT '',
-`uri` varchar(255) NOT NULL DEFAULT '',
-PRIMARY KEY (`id`,`image`)
-) ENGINE=MyISAM AUTO_INCREMENT=10 DEFAULT CHARSET=utf8
+	`id` mediumint(8) unsigned NOT NULL AUTO_INCREMENT,
+	`release_date` date NOT NULL DEFAULT '0000-00-00',
+	`title` varchar(255) NOT NULL DEFAULT '',
+	`author` varchar(255) NOT NULL DEFAULT '',
+	`source` varchar(255) NOT NULL,
+	`content` text NOT NULL,
+	`category` int(10) unsigned NOT NULL DEFAULT '0',
+	`image` varchar(255) NOT NULL DEFAULT '',
+	`uri` varchar(255) NOT NULL DEFAULT '',
+	`unique_visitors` MEDIUMINT(8) UNSIGNED NOT NULL,
+	`total_visitors` MEDIUMINT(8) UNSIGNED NOT NULL,
+	`like` MEDIUMINT(8) UNSIGNED NOT NULL,
+	`dislike` MEDIUMINT(8) UNSIGNED NOT NULL,
+	PRIMARY KEY (`id`),
+	KEY `unique_visitors` (`unique_visitors`),
+	KEY `total_visitors` (`total_visitors`),
+	KEY `like` (`like`),
+	KEY `dislike` (`dislike`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
---
+++ var/ui/www_article_rating/www_article_rating.ui.php
@@ -0,0 +1,90 @@
+<?php
+/**
+*	ПИ "Статьи: Рэйтинг"
+*
+* @author	Anthon S. Litvinenko <a.litvinenko@web50.ru>
+* @package	SBIN Diesel
+*/
+class ui_www_article_rating extends user_interface
+{
+	public $title = 'Статьи: Рэйтинг';
+
+	protected $deps = array(
+	);
+	
+	public function __construct ()
+	{
+		parent::__construct(__CLASS__);
+		$this->files_path = dirname(__FILE__).'/'; 
+	}
+
+	protected function pub_rating_bar()
+	{
+		$data = $this->get_args();
+		$template = $this->get_args('tmpl', 'rating_bar.html');
+                return $this->parse_tmpl($template, $data);
+	}
+
+	protected function reload_bar($id)
+	{
+		$di = data_interface::get_instance('article');
+		$di->what = array('id', 'like', 'dislike');
+		$di->set_args(array('_sid' => $id));
+		$di->_get();
+		$data = $di->get_results(0);
+		$template = $this->get_args('tmpl', 'reload_bar.html');
+                return $this->parse_tmpl($template, $data);
+	}
+
+	/**
+	*	Статья нравится
+	*/
+	protected function pub_like()
+	{
+		$id = request::get('x');
+		if (data_interface::get_instance('www_article_rating')->register_action($id, 1))
+		{
+			response::send(array(
+				'success' => true,
+				'bar' => $this->reload_bar($id),
+			), 'json');
+		}
+		else
+		{
+			response::send(array(
+				'success' => true,
+			), 'json');
+		}
+	}
+
+	/**
+	*	Статья не нравится
+	*/
+	protected function pub_dislike()
+	{
+		$id = request::get('x');
+		if (data_interface::get_instance('www_article_rating')->register_action($id, 0))
+		{
+			response::send(array(
+				'success' => true,
+				'bar' => $this->reload_bar($id),
+			), 'json');
+		}
+		else
+		{
+			response::send(array(
+				'success' => true,
+			), 'json');
+		}
+	}
+	
+	/**
+	*       Page configure form
+	*/
+	protected function sys_configure_form()
+	{
+		$tmpl = new tmpl($this->pwd() . 'configure_form.js');
+		response::send($tmpl->parse($this), 'js');
+	}
+}
+?>

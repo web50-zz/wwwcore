73354844370371646b27d55372527aa39f8be66e
Anthon S Litvinenko <a.litvinenko@web50.ru>
2013-12-09 20:26:48 +0400

    Добавлен новый модуль WWW Видео

---
+++ var/ui/www_video/www_video.ui.php
@@ -0,0 +1,64 @@
+<?php
+/**
+*	ПИ "WWW: Видео"
+*
+* @author	Anthon S. Litvinenko <a.litvinenko@web50.ru>
+* @package	SBIN Diesel
+*/
+class ui_www_video extends user_interface
+{
+	public $title = 'WWW: Видео';
+
+	protected $deps = array(
+		'main' => array(
+			'www_video.grid',
+		)
+	);
+	
+	public function __construct ()
+	{
+		parent::__construct(__CLASS__);
+		$this->files_path = dirname(__FILE__).'/'; 
+	}
+        
+        /**
+        *       Отрисовка контента для внешней части
+        */
+        public function pub_content()
+        {
+		$template = $this->get_args('tmpl', 'content.html');
+                $di = data_interface::get_instance('www_video');
+		$di->set_args($this->get_args());
+		$data = $di->get();
+		$data = array_merge($data, $this->args);
+                return $this->parse_tmpl($template, $data);
+        }
+	
+	/**
+	*       Управляющий JS админки
+	*/
+	public function sys_main()
+	{
+		$tmpl = new tmpl($this->pwd() . 'main.js');
+		response::send($tmpl->parse($this), 'js');
+	}
+	
+	/**
+	*       Управляющий JS админки
+	*/
+	public function sys_grid()
+	{
+		$tmpl = new tmpl($this->pwd() . 'grid.js');
+		response::send($tmpl->parse($this), 'js');
+	}
+	
+	/**
+	*       ExtJS - Форма редактирования
+	*/
+	public function sys_item_form()
+	{
+		$tmpl = new tmpl($this->pwd() . 'item_form.js');
+		response::send($tmpl->parse($this), 'js');
+	}
+}
+?>

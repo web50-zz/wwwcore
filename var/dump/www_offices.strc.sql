35758938cf125a1956accdd306a459e8e7cef1fd
Fedot B Pozdnyakov 9* <9@u9.ru>
2014-02-13 02:01:00 +0400

    www_office  UI/DI  Для ведения адресов офисов в котнактах и проч добавлен

---
+++ var/dump/www_offices.strc.sql
@@ -0,0 +1,17 @@
+DROP TABLE IF EXISTS `www_offices`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `www_offices` (
+  `id` mediumint(8) unsigned NOT NULL AUTO_INCREMENT,
+  `title` varchar(255) NOT NULL,
+  `content` text NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `site` varchar(255) NOT NULL,
+  `addr` text NOT NULL,
+  `postaddr` text NOT NULL,
+  `phones` text NOT NULL,
+  `work_time` text NOT NULL,
+  `map` text NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=6 DEFAULT CHARSET=utf8;
+
---
+++ var/ui/www_offices/www_offices.ui.php
@@ -0,0 +1,77 @@
+<?php
+/**
+*	ПИ "ARV: Акци"
+*
+* @author	Anthon S. Litvinenko <a.litvinenko@web50.ru>
+* @package	SBIN Diesel
+*/
+class ui_www_offices extends user_interface
+{
+	public $title = 'ARV: Акци';
+
+	protected $deps = array(
+		'main' => array(
+			'www_offices.grid',
+		)
+	);
+	
+	public function __construct ()
+	{
+		parent::__construct(__CLASS__);
+		$this->files_path = dirname(__FILE__).'/'; 
+	}
+        
+        /**
+        *       Отрисовка контента для внешней части
+        */
+        public function pub_content()
+        {
+		$template = $this->get_args('tmpl', 'content.html');
+                $data = array(
+			'records' => data_interface::get_instance('www_offices')
+				->push_args(array())
+				->set_order('id', 'DESC')
+				->_get()
+				->pop_args()
+				->get_results()
+		);
+                return $this->parse_tmpl($template, $data);
+        }
+	
+	/**
+	*       Управляющий JS админки
+	*/
+	public function sys_main()
+	{
+		$tmpl = new tmpl($this->pwd() . 'main.js');
+		response::send($tmpl->parse($this), 'js');
+	}
+	
+	/**
+	*       Управляющий JS админки
+	*/
+	public function sys_grid()
+	{
+		$tmpl = new tmpl($this->pwd() . 'grid.js');
+		response::send($tmpl->parse($this), 'js');
+	}
+	
+	/**
+	*       ExtJS - Форма редактирования
+	*/
+	public function sys_item_form()
+	{
+		$tmpl = new tmpl($this->pwd() . 'item_form.js');
+		response::send($tmpl->parse($this), 'js');
+	}
+	
+	/**
+	*       Page configure form
+	*/
+	protected function sys_configure_form()
+	{
+		$tmpl = new tmpl($this->pwd() . 'configure_form.js');
+		response::send($tmpl->parse($this), 'js');
+	}
+}
+?>

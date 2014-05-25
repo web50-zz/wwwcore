66952f5afc4513a8709bfd556e61376aef3117e6
Fedot B Pozdnyakov 9* <9@u9.ru>
2014-05-25 20:45:08 +0400

    Дамп стурктуры  последнийх изменений

--- var/dump/www_client.strc.sql
+++ var/dump/www_client.strc.sql
@@ -6,6 +6,7 @@ CREATE TABLE `www_client` (
 	`link` varchar(255) NOT NULL,
 	`description` text NOT NULL,
 	`photoalbum_id` int(10) unsigned NOT NULL,
+	`location_id` int(10) unsigned NOT NULL,
 	PRIMARY KEY (`id`),
 	KEY `order` (`order`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8;

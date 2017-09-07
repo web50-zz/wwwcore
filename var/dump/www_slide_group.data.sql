ec8f719b6955556d75af4c728b66b31513b897b7
Fedot B Pozdnyakov 9* <9@u9.ru>
2017-09-07 12:49:23 +0300

    Убрано лишнее в другой  инстанс

--- var/dump/www_slide_group.data.sql
+++
@@ -1,5 +0,0 @@
-LOCK TABLES `www_slide_group` WRITE;
-/*!40000 ALTER TABLE `www_slide_group` DISABLE KEYS */;
-INSERT INTO `www_slide_group` (`title`, `left`, `right`, `level`) VALUES ('root', 1, 2, 1);
-/*!40000 ALTER TABLE `www_slide_group` ENABLE KEYS */;
-UNLOCK TABLES;
